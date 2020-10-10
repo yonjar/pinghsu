ef188200a992734d9474b2205a7d7693632499dd
yonjar <1332934648@qq.com>
2020-10-10 13:59:55 +0800

    修改样式

--- header.php
+++ header.php
@@ -2,6 +2,61 @@
 <!DOCTYPE html>
 <html lang="zh-cmn-Hans">
 <head>
+    <title><?php $this->archiveTitle(array(
+    'category'  =>  _t(' %s '),
+    'search'    =>  _t(' %s '),
+    'tag'       =>  _t(' %s '),
+    'author'    =>  _t(' %s '),
+    'date'      =>  _t(' %s ')
+    ), '', ' - '); ?><?php $this->options->title(); ?></title>
+    <meta name="keywords" content="<?php $this->keywords(); ?>" />
+    <!-- OG Tags 首页输出 -->
+    <?php if($this->is('index')): ?>
+    <meta property="og:url" content="<?php $this->options->siteUrl(); ?>"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:title" content="<?php $this->options->title() ?>"/>
+    <meta property="og:image" content="https://yuika.love/img/icon.png"/>
+    <meta property="og:author" content="LautlosP"/>
+    <meta property="og:site_name" content="<?php $this->options->title() ?>"/>
+    <meta property="og:description" content="<?php $this->options->title() ?> 为DD而生"/>
+    <meta property="og:locale:alternate" content="zh_CN"/>
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@imyonjar">
+    <meta name="twitter:title" content="<?php $this->options->title() ?>">
+    <meta name="twitter:description" content="<?php $this->options->title() ?> 为DD而生">
+    <meta name="twitter:url" content="<?php $this->options->siteUrl(); ?>">
+    <meta name="twitter:image" content="https://yuika.love/img/icon.png">
+    <?php endif; ?>
+
+    <!-- OG Tags 文章和独立页面输出 -->
+    <?php if($this->is('post')||$this->is('page')): ?>
+    <?php $thumb = showThumb($this,null,true);?>
+    <meta property="og:url" content="<?php $this->permalink(); ?>"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:title" content="<?php $this->title(); ?>"/>
+        <?php if(!empty($thumb)):?>
+        <meta property="og:image" content="<?php echo $thumb ?>"/>
+        <?php else: ?>
+        <meta property="og:image" content="https://yuika.love/img/icon.png"/>
+        <?php endif; ?>
+    <meta property="og:author" content="<?php $this->author(); ?>"/>
+    <meta property="og:site_name" content="<?php $this->options->title(); ?>"/>
+    <meta property="og:description" content="<?php $this->description(); ?>"/>
+    <meta property="og:locale:alternate" content="zh_CN"/>
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@imyonjar">
+    <meta name="twitter:title" content="<?php $this->title(); ?>">
+    <meta name="twitter:description" content="<?php $this->description(); ?>">
+    <meta name="twitter:url" content="<?php $this->permalink(); ?>">
+        <?php if(!empty($thumb)):?>
+        <meta name="twitter:image" content="<?php echo $thumb ?>"/>
+        <?php else: ?>
+        <meta name="twitter:image" content="https://yuika.love/img/icon.png"/>
+        <?php endif; ?>
+    <?php endif; ?>
+    <!-- OG Tags end -->
     <meta charset="<?php $this->options->charset(); ?>"><?php if ($this->options->DnsPrefetch): ?>
     <meta http-equiv="x-dns-prefetch-control" content="on"><?php if ($this->options->cdn_add): ?>
     <link rel="dns-prefetch" href="<?php $this->options->cdn_add(); ?>" /><?php endif; ?>
@@ -16,17 +71,10 @@
     <link href="https://cdn.bootcss.com/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">    <?php if($this->options->favicon): ?>
     <link rel="shortcut icon" href="<?php $this->options->favicon(); ?>"><?php endif;?><?php if($this->options->iosicon): ?>
     <link rel="apple-touch-icon" href="<?php $this->options->iosicon();?>"><?php endif; ?>
-    <title><?php $this->archiveTitle(array(
-    'category'  =>  _t(' %s '),
-    'search'    =>  _t(' %s '),
-    'tag'       =>  _t(' %s '),
-    'author'    =>  _t(' %s '),
-    'date'      =>  _t(' %s ')
-    ), '', ' - '); ?><?php $this->options->title(); ?></title>
-    <meta name="keywords" content="<?php $this->keywords(); ?>" />
     <?php $this->header('keywords=&generator=&template=&pingback=&xmlrpc=&wlw=&commentReply=&rss1=&rss2=&atom='); ?>
-    <link href="<?php $this->options->themeUrl('css/style.min.css?20170331'); ?>" rel="stylesheet">
+    <link href="<?php $this->options->themeUrl('css/style.min.css'); ?>" rel="stylesheet">
     <link href="<?php $this->options->themeUrl('css/my.style.min.css'); ?>" rel="stylesheet">
+ 
     <!--[if lt IE 9]>
     <script src="//cdn.bootcss.com/html5shiv/r29/html5.min.js"></script>
     <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
